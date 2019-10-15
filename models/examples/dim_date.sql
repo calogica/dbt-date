0e9ca7932adc524474c1b92ba779ddf942ab0574
Claus Herther <claus@calogica.com>
2019-10-15 14:25:25 -0700

    disables example models

--- models/examples/dim_date.sql
+++ models/examples/dim_date.sql
@@ -1,6 +1,7 @@
 {{
     config(
-        materialized = 'table'
+        materialized = 'table',
+        enabled = false
     )
 }}
 with date_dimension as (
--- models/examples/dim_date_fiscal.sql
+++ models/examples/dim_date_fiscal.sql
@@ -1,6 +1,7 @@
 {{
     config(
-        materialized = 'table'
+        materialized = 'table',
+        enabled = false
     )
 }}
 with fp as (
