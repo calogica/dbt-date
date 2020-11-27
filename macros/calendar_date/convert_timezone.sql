dd2c5c2013abcb6ccc8f98a38a08b2a2aa5542e4
clausherther <claus@calogica.com>
2020-11-27 16:11:23 -0700

    Adds spark support for convert_timezone

--- macros/calendar_date/convert_timezone.sql
+++ macros/calendar_date/convert_timezone.sql
@@ -1,16 +1,24 @@
 {%- macro convert_timezone(column, target_tz=None, source_tz=None) -%}
+{%- set source_tz = "UTC" if not source_tz else source_tz -%}
 {%- set target_tz = var("dbt_date:time_zone") if not target_tz else target_tz -%}
 {{ adapter.dispatch('convert_timezone', packages = dbt_date._get_utils_namespaces()) (column, target_tz, source_tz) }}
 {%- endmacro -%}
 
 {% macro default__convert_timezone(column, target_tz, source_tz) -%}
-    {%- if not source_tz -%}
-    cast(convert_timezone('{{ target_tz }}', {{ column }}) as {{ dbt_utils.type_timestamp() }})
-    {%- else -%}
-    cast(convert_timezone('{{ source_tz }}', '{{ target_tz }}', {{ column }}) as {{ dbt_utils.type_timestamp() }})
-    {%- endif -%}
+{%- if not source_tz -%}
+cast(convert_timezone('{{ target_tz }}', {{ column }}) as {{ dbt_utils.type_timestamp() }})
+{%- else -%}
+cast(convert_timezone('{{ source_tz }}', '{{ target_tz }}', {{ column }}) as {{ dbt_utils.type_timestamp() }})
+{%- endif -%}
 {%- endmacro -%}
 
 {%- macro bigquery__convert_timezone(column, target_tz, source_tz=None) -%}
-    timestamp(datetime({{ column }}, '{{ target_tz}}'))
+timestamp(datetime({{ column }}, '{{ target_tz}}'))
+{%- endmacro -%}
+
+{%- macro spark__convert_timezone(column, target_tz, source_tz) -%}
+from_utc_timestamp(
+        to_utc_timestamp({{ column }}, '{{ source_tz }}'),
+        '{{ target_tz }}'
+        )
 {%- endmacro -%}
