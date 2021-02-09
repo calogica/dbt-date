9a5a6944a1e45f0bc48e29f373a5a269f2bf144a
Han Hong <seung-han.hong@hotmail.com>
2021-02-09 15:33:54 +1100

    adding support for postgres

--- macros/calendar_date/week_start.sql
+++ macros/calendar_date/week_start.sql
@@ -16,3 +16,8 @@ case
             ) }}
 end
 {%- endmacro %}
+
+{%- macro postgres__week_start(date) -%}
+-- Sunday as week start date
+cast({{ dbt_utils.dateadd('day', -1, dbt_utils.date_trunc('week', dbt_utils.dateadd('day', 1, date))) }} as date)
+{%- endmacro %}
