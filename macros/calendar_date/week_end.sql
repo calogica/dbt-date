4e0b06d34da7655c4956ccb429da564f84a4e950
Anders Swanson <swanson.anders@gmail.com>
2021-04-28 18:04:48 -0700

    redundant bc dbt-utils has postgres__last_day()

--- macros/calendar_date/week_end.sql
+++ macros/calendar_date/week_end.sql
@@ -11,8 +11,3 @@
 {%- set dt = dbt_date.week_start(date) -%}
 {{ dbt_date.n_days_away(6, dt) }}
 {%- endmacro %}
-
-{%- macro postgres__week_end(date) -%}
-{%- set dt = dbt_date.week_start(date) -%}
-{{ dbt_date.n_days_away(6, dt) }}
-{%- endmacro %}
