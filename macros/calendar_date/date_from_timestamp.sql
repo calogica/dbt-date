896112876b966b0677c71ab25d6a65f5d3279cf6
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2022-11-07 09:03:09 -0900

    updating macro reference

--- macros/calendar_date/date_from_timestamp.sql
+++ macros/calendar_date/date_from_timestamp.sql
@@ -1,15 +1,15 @@
-{% macro get_date_from_timestamp(timestamp) %}
-    {{ return(adapter.dispatch('get_date_from_timestamp','dbt_date') (timestamp)) }}
+{% macro date_from_timestamp(timestamp) %}
+    {{ return(adapter.dispatch('date_from_timestamp','dbt_date') (timestamp)) }}
 {% endmacro %}
 
-{% macro default__get_date_from_timestamp(timestamp) %}
+{% macro default__date_from_timestamp(timestamp) %}
     date({{ timestamp }})
 {% endmacro %}
 
-{% macro bigquery__get_date_from_timestamp(timestamp) %}
+{% macro bigquery__date_from_timestamp(timestamp) %}
     cast({{ timestamp }} as date)
 {% endmacro %}
 
-{% macro snowflake__get_date_from_timestamp(timestamp) %}
+{% macro snowflake__date_from_timestamp(timestamp) %}
     to_date(to_timestamp({{ timestamp }}))
 {% endmacro %}
