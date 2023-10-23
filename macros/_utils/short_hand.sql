b03b20509f5fd7c21c19c147d5780041c725d1ca
Claus Herther <claus@calogica.com>
2023-10-23 16:21:04 -0700

    Rename macro file

--- macros/_utils/short_hand.sql
+++
@@ -1,7 +0,0 @@
-{% macro date(year, month, day) %}
-    {{ return(modules.datetime.date(year, month, day)) }}
-{% endmacro %}
-
-{% macro datetime(year, month, day, hour=0, minute=0, second=0, millisecond=0) %}
-    {{ return(modules.datetime.datetime(year, month, day, hour, minute, second, millisecond, modules.pytz.timezone(var("dbt_date:time_zone")))) }}
-{% endmacro %}
